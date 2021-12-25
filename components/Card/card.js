00b012b02c864829235eeea83537da02b516a555
boa <boa.matule@gmail.com>
2021-12-25 19:14:07 +0100

    Uses unplash image as a default for image error handling

--- components/Card/card.js
+++ components/Card/card.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styles from './card.module.css';
 
 const Card = (props) => {
-	const { imgUrl = "/static/chingu.webp", size = "medium"} = props;
+	const { imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW92aWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60", size = "medium"} = props;
 
 	const [imgSrc, setImgSrc] = useState(imgUrl);
 	
@@ -14,7 +14,7 @@ const Card = (props) => {
 	}
 
 	const handleOnError = () => {
-		setImgSrc("/static/chingu.webp");
+		setImgSrc("https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW92aWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60");
 	};
 
 	return (
