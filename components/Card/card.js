46aaae6e02d5c6d288859028502b3197a3539c30
boa <boa.matule@gmail.com>
2021-12-25 18:57:16 +0100

    Implements handleOnError function to handle the image failure;

--- components/Card/card.js
+++ components/Card/card.js
@@ -1,21 +1,30 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import styles from './card.module.css';
 
 const Card = (props) => {
-	const { imgUrl, size } = props;
+	const { imgUrl = "/static/chingu.webp", size = "medium"} = props;
 
+	const [imgSrc, setImgSrc] = useState(imgUrl);
+	
 	const classMap = {
 		'large': styles.lgItem,
 		'medium': styles.mdItem,
 		'small': styles.smItem,
 	}
+
+	const handleOnError = () => {
+		setImgSrc("/static/chingu.webp");
+	};
+
 	return (
 		<div className={styles.container}> 
 			<div className={classMap[size]}>
 			<Image 
-				src={imgUrl}
+				src={imgSrc}
 				alt="image"
 				layout="fill"
+				onError={handleOnError}
 				className={styles.cardImg}
 				/>
 			</div>
