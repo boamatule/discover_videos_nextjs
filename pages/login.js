a0fac6adb250cdea5fb171c5e1b18a2b8cc1fb28
boa <boa.matule@gmail.com>
2021-12-26 15:01:19 +0100

    Implements Sign-in Form Validatiion

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import Head from "next/head";
 import Image from "next/image";
 
 import styles from "../styles/Login.module.css";
 
 const Login = () => {
+	const [userMsg, setUserMsg] = useState('');
+	const [email, setEmail] = useState('');
+
+	const handleOnChangeEmail = (e) => {
+		setUserMsg();
+		console.log("event", e);
+		const email = e.target.value;
+		setEmail(email);
+		e.preventDefault();
+	};
+
 	const handleLoginWithEmail = (e) => {
 		console.log("Hi button");
 		e.preventDefault();
-	}
+		if (email) {
+			// route to dashboard
+		} else {
+			// show user message
+			setUserMsg('Enter a valid email address')
+		}
+	};
   return (
     <div className={styles.container}>
       <Head>
@@ -35,8 +53,9 @@ const Login = () => {
 					type="text" 
 					placeholder="Email address"
 					className={styles.emailInput}
+					onChange={handleOnChangeEmail}
 					/>
-				<p className={styles.userMsg}></p>
+				<p className={styles.userMsg}>{userMsg}</p>
 				<button
 					onClick={handleLoginWithEmail}
 					className={styles.loginBtn}
