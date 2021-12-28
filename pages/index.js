ca6532dc89ff2ffa5caa8a67499a3f83b6f58cbb
boa <boa.matule@gmail.com>
2021-12-28 18:45:39 +0100

    Query live hasura Server with Environment Variables

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,6 @@ import styles from "../styles/Home.module.css";
 
 import { getPopularVideos, getVideos } from "../lib/videos";
 
-import { startFetchMyQuery } from "../lib/db/hasura";
-
 export async function getServerSideProps() {
   const disneyVideos = await getVideos("disney trailer");
   const productivityVideos = await getVideos("productivity");
@@ -29,7 +27,6 @@ export default function Home({
   travelVideos,
   popularVideos,
 }) {
-  startFetchMyQuery();
   return (
     <div className={styles.container}>
       <Head>
