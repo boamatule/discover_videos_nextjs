6f097b4927b903d02c3e3199bc3c5513cd4ba470
boa <boa.matule@gmail.com>
2022-01-07 22:15:43 +0100

    Creates an Icon folder for the like and dislike button and styles it. WIP

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,24 +1,25 @@
 import { useRouter } from "next/router";
 import Modal from "react-modal";
 import styles from "../../styles/Video.module.css";
-import clsx  from "classnames";
+import clsx from "classnames";
 import { getYoutubeVideoById } from "../../lib/videos";
 import NavBar from "../../components/nav/navbar";
-
+import DisLike from "../../components/icons/dislike-icon";
+import Like from "../../components/icons/like-icon";
 
 Modal.setAppElement("#__next");
 
 export async function getStaticProps(context) {
-	const videoId = context.params.videoId;
+  const videoId = context.params.videoId;
 
-	const videoArray = await getYoutubeVideoById(videoId);
+  const videoArray = await getYoutubeVideoById(videoId);
 
   return {
     props: {
       video: videoArray.length > 0 ? videoArray[0] : {},
     },
     revalidate: 10, // In seconds
-  }
+  };
 }
 
 export async function getStaticPaths() {
@@ -28,9 +29,8 @@ export async function getStaticPaths() {
     params: { videoId },
   }));
 
-  return { paths, fallback: 'blocking' }
+  return { paths, fallback: "blocking" };
 }
-
 
 const Video = ({ video }) => {
   const router = useRouter();
@@ -50,7 +50,7 @@ const Video = ({ video }) => {
         isOpen={true}
         contentLabel="Watch the video"
         onRequestClose={() => router.back()}
-				className={styles.modal}
+        className={styles.modal}
         overlayClassName={styles.overlay}
       >
         <iframe
@@ -61,9 +61,22 @@ const Video = ({ video }) => {
           height="360"
           src={`http://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://example.com&controls=0&rel=0`}
           frameBorder="0"
-          >
-        </iframe>
-				
+        ></iframe>
+        <div className={styles.likeDislikeBtnWrapper}>
+        <div className={styles.likeBtnWrapper}>
+          <button>
+            <div className={styles.btnWrapper}>
+              <Like />
+            </div>
+          </button>
+          </div>
+          <button>
+            <div className={styles.btnWrapper}>
+              <DisLike />
+            </div>
+          </button>
+        </div>
+
         <div className={styles.modalBody}>
           <div className={styles.modalBodyContent}>
             <div className={styles.col1}>
