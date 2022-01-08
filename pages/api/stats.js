7a8aff9c05315ee593cd4be5f396fe16b4e6201b
boa <boa.matule@gmail.com>
2022-01-08 11:54:46 +0100

    Builds the Stats API further requesting cookies token.WIP

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,5 +1,16 @@
 export default async function stats(req, resp) {
-	if (req.method === "POST") {
-		resp.send({ msg: "It works" });
-	}
+  if (req.method === "POST") {
+    console.log({ cookies: req.cookies });
+
+    try {
+      if (!req.cookies.token) {
+        resp.status(403).send({});
+      } else {
+        resp.send({ msg: "It works" });
+      }
+    } catch (error) {
+			console.log('Error occured /stats', error)
+      resp.status(500).send({ done: false, error: error?.message });
+    }
+  }
 }
