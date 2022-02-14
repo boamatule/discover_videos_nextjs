4fe890c966fcc9d48c63dc250a23a8839c6e1aad
boa <boa.matule@gmail.com>
2022-02-14 18:17:40 +0100

    Inserts and updates mutations in our stats API; Tests the Stats API and all working.

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,39 +1,43 @@
-import jwt from 'jsonwebtoken';
-import {
-  findVideoIdByUser,
-} from "../../lib/db/hasura";
-
+import jwt from "jsonwebtoken";
+import { findVideoIdByUser, updateStats } from "../../lib/db/hasura";
 
 export default async function stats(req, resp) {
-  if (req.method === "POST") {
-    // console.log({ cookies: req.cookies });
+	if (req.method === "POST") {
+		console.log({ cookies: req.cookies });
 
-    try {
+		try {
 			const token = req.cookies.token;
-      if (!token) {
-        resp.status(403).send({});
-      } else {
-        const videoId = req.query.videoId;
+			if (!token) {
+				resp.status(403).send({});
+			} else {
+				const videoId = req.query.videoId;
 				const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
 				// console.log({ decodedToken })
 
-        const userId = decodedToken.issuer;
-        const doesStatsExists = await findVideoIdByUser
-        (token, userId, videoId);
-        console.log({doesStatsExists })
-  
-        if (doesStatsExists) {
-          // update it
-          
-        } else {
-          // add it
-        }
-        resp.send({ msg: "It works", decodedToken, doesStatsExists });
-      }
-    } catch (error) {
-			console.log('Error occured /stats', error)
-      resp.status(500).send({ done: false, error: error?.message });
-    }
-  }
+				const userId = decodedToken.issuer;
+				const doesStatsExists = await findVideoIdByUser(token, userId, videoId);
+				console.log({ doesStatsExists });
+
+				if (doesStatsExists) {
+					// update it
+					const response = await updateStats(token, {
+            watched: false,
+            userId,
+            videoId: 'CaimKeDcudo',
+            favourited: 0,
+          });
+					// resp.send({ msg: "It works", decodedToken, doesStatsExists });
+          resp.send({ msg: "It works", response });
+				} else {
+					// add it
+          resp.send({ msg: "It works", decodedToken, doesStatsExists });
+
+				}
+			}
+		} catch (error) {
+			console.log("Error occured /stats", error);
+			resp.status(500).send({ done: false, error: error?.message });
+		}
+	}
 }
