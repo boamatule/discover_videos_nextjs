552a17b287347dd736ef5d909d8c43a04f517a31
boa <boa.matule@gmail.com>
2022-04-23 21:09:58 +0200

    Creates a Redirect User Hook

--- utils/redirectUser.js
+++ utils/redirectUser.js
@@ -1,8 +1,26 @@
 import { verifyToken } from "../lib/utils";
 
 const useRedirectUser = async (context) => {
-	const token = context.req ? context.req.cookies?.token : null;
-	const userId = verifyToken(token);
+	// const token = context.req ? context.req.cookies?.token : null;
+	// const userId = verifyToken(token);
+
+	// return {
+	// 	userId,
+	// 	token,
+	// };
+
+	const token = context.req ? context.req?.cookies.token : null;
+	const userId = await verifyToken(token);
+
+	if (!userId) {
+		return {
+			props: {},
+			redirect: {
+				destination: "/login",
+				permanent: false,
+			},
+		};
+	}
 	
 	return {
 		userId,
