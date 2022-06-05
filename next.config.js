7ac3010a2326623cd4345bcb7163800913ea3511
boa <boa.matule@gmail.com>
2022-06-05 20:58:33 +0200

    Configures the next config file to enable debug logging to see the errors about missing modules clearly.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  webpack(config) {
+    config.infrastructureLogging = { debug: /PackFileCache/ }
+    return config;
+  },
   images: {
     domains: ["images.unsplash.com", "i.ytimg.com"]
   }
