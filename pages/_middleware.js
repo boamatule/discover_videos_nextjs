150a32aca0220e1ed6039a104609ebe68cb6e576
boa <boa.matule@gmail.com>
2022-06-24 10:49:37 +0200

    Modifies the Middleware to clone NextURL and mutates it to allow redirecting and rewriting

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,12 +2,13 @@ import { NextResponse } from "next/server";
 import { verifyToken } from "../lib/utils";
 
 export async function middleware(req, ev) {
-  console.log({req, ev })
-  const token = req ? req.cookies?.token : null;
-  const userId = await verifyToken(token);
+	console.log({ req, ev });
+	const token = req ? req.cookies?.token : null;
+	const userId = await verifyToken(token);
 
-	if (!token && pathname !== "/login") {
-    console.log("redirected")
-		return NextResponse.redirect(new URL("/login", req.url));
+	if ((!token || !userId) && pathname !== "/login") {
+		const url = req.nextUrl.clone();
+		url.pathname = "/login";
+		return NextResponse.rewrite(url);
 	}
 }
