c244b592327bc859ad869c4feb44b166872e6263
boa <boa.matule@gmail.com>
2022-06-26 21:24:23 +0200

    fix React Hook warnings for async functions in useEffect in vide file.

--- middleware.js
+++ middleware.js
@@ -1,15 +1,24 @@
+/* eslint-disable @next/next/no-server-import-in-page */
 import { NextResponse } from "next/server";
 import { verifyToken } from "./lib/utils";
 
+export async function middleware(req) {
+  const token = req ? req.cookies?.token : null;
+  const userId = await verifyToken(token);
 
-export async function middleware(req, ev, NextResponse) {
-	console.log({ req, ev });
-	const token = req ? req.cookies?.token : null;
-	const userId = await verifyToken(token);
+  const { pathname } = req.nextUrl;
 
-	if ((!token || !userId) && pathname !== "/login") {
-		const url = req.nextUrl.clone();
-		url.pathname = "/login";
-		return NextResponse.rewrite(url);
-	}
+  if (
+    pathname.includes("/api/login") ||
+    userId ||
+    pathname.includes("/static")
+  ) {
+    return NextResponse.next();
+  }
+
+  if ((!token || !userId) && pathname !== "/login") {
+    const url = req.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.rewrite(url);
+  }
 }
