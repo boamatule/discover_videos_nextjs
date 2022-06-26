f147f8b797caa6a2f02c26d63f5e4b8f0aca8f23
boa <boa.matule@gmail.com>
2022-06-26 21:56:51 +0200

    fix React Hook warnings for async functions in useEffect for the NavBar component.

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -1,35 +1,39 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router'
-import Image from 'next/image';
-import styles from './navbar.module.css';
-import { magic } from '../../lib/magic-client';
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./navbar.module.css";
+
+import { magic } from "../../lib/magic-client";
 
 const Navbar = () => {
 	const [showDropdown, setShowDropdown] = useState(false);
-
-	const [username, setUsername] = useState('');
-
+	const [username, setUsername] = useState("");
+	const [didToken, setDidToken] = useState("");
 	const router = useRouter();
 
-	useEffect(async function () {
-		try {
-			const { email } = await magic.user.getMetadata();
-			const didToken = await magic.user.getIdToken();
-			if (email) {
-				setUsername(email);
+	useEffect(() => {
+		const fetchUserData = async () => {
+			try {
+				const { email } = await magic.user.getMetadata();
+				const didToken = await magic.user.getIdToken();
+				if (email) {
+					setUsername(email);
+				}
+			} catch (error) {
+				console.error("Error retrieving email", error);
 			}
-		} catch (error) {
-			console.error('Error retrieving email', error);
-		}
+		};
+		fetchUserData;
 	}, []);
 
 	function handleOnClickHome(e) {
 		e.preventDefault();
-		router.push('/');
+		router.push("/");
 	}
 	const handleOnClickMyList = (e) => {
 		e.preventDefault();
-    router.push('browse/my-list/');
+		router.push("browse/my-list/");
 	};
 
 	const handleShowDropdown = (e) => {
@@ -37,79 +41,74 @@ const Navbar = () => {
 		setShowDropdown(!showDropdown);
 	};
 
-  const handleSignout = async (e) => {
-    e.preventDefault();
+	const handleSignout = async (e) => {
+		e.preventDefault();
 
-    try {
-      const response = await fetch("/api/logout", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${didToken}`,
-          "Content-Type": "application/json",
-        },
-      });
+		try {
+			const response = await fetch("/api/logout", {
+				method: "POST",
+				headers: {
+					Authorization: `Bearer ${didToken}`,
+					"Content-Type": "application/json",
+				},
+			});
 
-      const res = await response.json();
-    } catch (error) {
-      console.error("Error logging out", error);
-      router.push("/login");
-    }
-  };
+			const res = await response.json();
+		} catch (error) {
+			console.error("Error logging out", error);
+			router.push("/login");
+		}
+	};
 
 	return (
-	<div className={styles.container}>
-		<div className={styles.wrapper}>
-			<a className={styles.logoLink}>
-				<div className={styles.logoWrapper}>
-					<Image 
-						src="/static/netflix.svg"
-						width="128"
-						height="34"
-						alt="Netflix logo" 
-					/>
-				</div>
-			</a>
-		
-	
-		<ul className={styles.navItems}>
-			<li className={styles.navItem} onClick={handleOnClickHome}>
-				Home
-				</li>
-				<li className={styles.navItem2} onClick={handleOnClickMyList}>
-					My List
-				</li>
-		</ul>
-		
-		<nav className={styles.navContainer}>
-			<div>
-				<button className={styles.usernameBtn}
-					onClick={handleShowDropdown}
-				>
-					<p className={styles.username}>{username}</p>
-					<Image 
-						src="/static/expand_more.svg"
-						width="24"
-						height="24"
-						alt="Expand dropdown" 
-					/>
-				</button>
-				{showDropdown && (
-				<div className={styles.navDropdown}>
+		<div className={styles.container}>
+			<div className={styles.wrapper}>
+				<a className={styles.logoLink}>
+					<div className={styles.logoWrapper}>
+						<Image
+							src="/static/netflix.svg"
+							width="128"
+							height="34"
+							alt="Netflix logo"
+						/>
+					</div>
+				</a>
+
+				<ul className={styles.navItems}>
+					<li className={styles.navItem} onClick={handleOnClickHome}>
+						Home
+					</li>
+					<li className={styles.navItem2} onClick={handleOnClickMyList}>
+						My List
+					</li>
+				</ul>
+
+				<nav className={styles.navContainer}>
 					<div>
-						<a 
-							className={styles.linkName} 	
-							onClick={handleSignout}
-							>
-								Sign Out
-							</a>
-					<div className={styles.lineWrapper}></div>
-				</div>
-				</div>
-				)}
+						<button className={styles.usernameBtn} onClick={handleShowDropdown}>
+							<p className={styles.username}>{username}</p>
+							<Image
+								src="/static/expand_more.svg"
+								width="24"
+								height="24"
+								alt="Expand dropdown"
+							/>
+						</button>
+						{showDropdown && (
+							<div className={styles.navDropdown}>
+								<div>
+									<a className={styles.linkName} onClick={handleSignout}>
+										Sign Out
+									</a>
+									<div className={styles.lineWrapper}></div>
+								</div>
+							</div>
+						)}
+					</div>
+				</nav>
 			</div>
-		</nav>
 		</div>
-	</div>)
-}
+	);
+};
 
 export default Navbar;
