144dbdcbefdfe997a9409b36fac5c8533e83935d
boa <boa.matule@gmail.com>
2022-07-18 13:54:44 +0200

    Site Optmization

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,9 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+        <meta name="Description" content="Kinda Nextflix" />
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 					<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap" rel="stylesheet" />
