7ef4a34f637a664118103618cc68cb9d0780fa42
boa <boa.matule@gmail.com>
2022-07-18 14:31:27 +0200

    Setups PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+// module.exports = {
+//   images: {
+//     domains: ["images.unsplash.com", "i.ytimg.com"],
+//   },
+// };
+
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
   images: {
     domains: ["images.unsplash.com", "i.ytimg.com"],
   },
-};
+});
--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,10 @@ class MyDocument extends Document {
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 					<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap" rel="stylesheet" />
-				</Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#fff" />
+        </Head>
         <body>
           <Main />
           <NextScript />
