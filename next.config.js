149f7845ffdea73f749b9be19ce6fd99555b74bc
boa <boa.matule@gmail.com>
2022-07-18 22:49:02 +0200

    Fixing the login in the deployed vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withPWA = require("next-pwa");
+const prod = process.env.NODE_ENV === 'production'
 
 // module.exports = {
 //   images: {
@@ -12,6 +13,7 @@ module.exports = withPWA({
     dest: "public",
     register: true,
     skipWaiting: true,
+    // disable: prod ? false : true,
   },
   images: {
     domains: ["images.unsplash.com", "i.ytimg.com"],
