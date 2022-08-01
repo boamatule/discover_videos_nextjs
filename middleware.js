9f358f9f47d8641a6323f2c99d42dcf7386cf020
boa <boa.matule@gmail.com>
2022-08-01 21:33:19 +0200

    Fix the Middleware

--- middleware.js
+++ middleware.js
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 import { verifyToken } from "./lib/utils";
 
-export async function middleware(req) {
-  const token = req ? req.cookies?.token : null;
+export async function middleware(req, ev) {
+  const token = req ? req.cookies.get("token") : null;
   const userId = await verifyToken(token);
-
   const { pathname } = req.nextUrl;
 
   if (
+    pathname.startsWith("/_next") ||
     pathname.includes("/api/login") ||
     userId ||
     pathname.includes("/static")
